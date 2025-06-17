d08d4f1f98df3941a5ceedca094b2291f05d7d5a
Annette <annette.ngugi@student.moringaschool.com>
2025-06-17 14:38:38 +0300

    added  seed data

---
+++ server/seed.py
@@ -0,0 +1,26 @@
+from .config import db
+from .app import create_app
+from .models.restaurant import Restaurant
+from .models.pizza import Pizza
+from .models.restaurant_pizza import RestaurantPizza
+
+app = create_app()
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    r1 = Restaurant(name="Kiki's Pizza", address="123 Pizza Street")
+    r2 = Restaurant(name="Mario's", address="456 Mushroom Ave")
+
+    p1 = Pizza(name="Emma", ingredients="Dough, Tomato Sauce, Cheese")
+    p2 = Pizza(name="Spicy Delight", ingredients="Dough, Tomato Sauce, Cheese, Pepperoni")
+
+    db.session.add_all([r1, r2, p1, p2])
+    db.session.commit()
+
+    rp1 = RestaurantPizza(price=5, pizza_id=p1.id, restaurant_id=r1.id)
+    rp2 = RestaurantPizza(price=8, pizza_id=p2.id, restaurant_id=r2.id)
+
+    db.session.add_all([rp1, rp2])
+    db.session.commit()
